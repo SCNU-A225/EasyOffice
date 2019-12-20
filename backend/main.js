c5da8be19d52695c96340eb6b33429abfa87f345
Machinec <1585645621@qq.com>
2019-12-20 23:39:37 +0800

    complete department and employee

--- backend/main.js
+++ backend/main.js
@@ -92,6 +92,12 @@ server.use(function(request,response,next){
     }
 */
 
+
+/*
+ ******************************
+ *个人信息管理
+ ****************************** 
+ */
 /**
  * 用户登录
  */
@@ -115,8 +121,10 @@ server.post('/login',function(request,response){
             request.session.sn = sn
             //员工姓名
             let name = result[0].name
+            request.session.name = name
             //员工职务
             let post = result[0].post
+            request.session.post = post
             //员工部门编号
             request.session.department_sn = result[0].department_sn
             let sql = 'select * from department where sn=?'
@@ -125,6 +133,7 @@ server.post('/login',function(request,response){
                 if(result.length > 0){
                     //员工部门
                     let department = result[0].name
+                    request.session.department = department
                     response.json({code:200, sn:sn, name:name, post:post, department:department})
                 }
             })
@@ -148,6 +157,30 @@ server.get('/logout',function(request, response){
     // 302 redirect: 302 代表暂时性转移(Temporarily Moved )，保留就地址内容，可以返回到跳转前的页面及状态
 })
 
+/**
+ * 查看个人信息
+ */
+server.get('/info',function(request, response){
+    response.json({
+        sn: request.session.sn,
+        name: request.session.name,
+        department: request.session.department,
+        post: request.session.post
+    })
+})
+
+/**
+ * 修改密码
+ */
+server.post('/password', function(request, response){
+    let sn = request.session.sn
+    let password = request.body.password
+    let sql = 'update employee set password=? where sn=?'
+    pool.query(sql, [password, sn], function(err, result){
+        if(err) throw err
+        response.json({code:200, msg:'修改成功'})
+    })
+})
 /*
  ******************************
  *部门管理
@@ -191,7 +224,186 @@ server.get('/logout',function(request, response){
 
  /**
   * 1.2删除部门
+  * 删除部门，同时部门的员工也要删除
+  * 先删除同部门的员工才能删除部门记录，否则外键约束检查发现部门记录与响应部门员工存在外键依赖无法修改或删除外键
   */
- server.get('/department/delete',function(request,response){
-     
+ server.post('/department/delete',function(request,response){
+     let sn = request.body.sn
+     let sql1 = 'delete from employee where department_sn=?'
+     pool.query(sql1, [sn], function(err, result){
+         if(err) throw err
+         let sql2 = 'delete from department where sn=?'
+         pool.query(sql2, [sn], function(err, result){
+             if(err) throw err
+             response.json({code:200, msg:'删除成功'})
+         })
+     })
+ })
+
+ /**
+  * 1.3查找所有部门
+  */
+ server.get('/department/all',function(request,response){
+     let sql = 'select * from department'
+     let resData = {departmentList:null}
+     pool.query(sql,function(err, result){
+         if(err) throw err
+         resData.departmentList = result
+         response.json(resData)
+     })
+ })
+
+ /**
+  * 1.4修改部门
+  */
+server.post('/department/update',function(request,response){
+    let sn = request.body.sn
+    let name = request.body.name
+    let address = request.body.address
+    if(name == ''){
+       response.json({code:401, msg:'请输入部门名称'})
+       return
+    }
+    if(address == ''){
+       response.json({code:402, msg:'请输入部门地址'})
+       return
+    }
+    let sql = 'update department set name=?, address=? where sn=?'
+    pool.query(sql, [name, address, sn], function(err, result){
+        if(err) throw err
+        response.json({cond:200, msg:'更新成功'})
+    })
+})
+
+/*
+ ******************************
+ *员工管理
+ ****************************** 
+ */
+
+ /**
+  * 2.1 添加员工
+  */
+ server.post('/employee/add',function(request,response){
+     let sn = request.body.sn
+     let name = request.body.name
+     let password = request.body.password
+     let department_sn = request.body.department_sn
+     let post = request.body.post
+     if(sn == ''){
+         response.json({code:401, msg:'请输入员工编号'})
+         return
+     }
+     if(name == ''){
+        response.json({code:402, msg:'请输入员工姓名'})
+        return
+    }
+    if(password == ''){
+        response.json({code:403, msg:'请输入密码'})
+        return
+    }
+    if(department_sn == ''){
+        response.json({code:404, msg:'请输入部门编号'})
+        return
+    }
+    if(post == ''){
+        response.json({code:405, msg:'请输入职务'})
+        return
+    }
+     let sql1 = 'select * from employee where sn=?'
+     pool.query(sql1, [sn], function(err, result){
+         if(err) throw err
+         if(result.length > 0){
+             response.json({code:501, msg:'员工编号已存在'})
+         }else{
+             let sql2 = 'select * from department where sn=?'
+             pool.query(sql2, [department_sn], function(err, result){
+                 if(result.length > 0){
+                    let sql3 = 'insert into employee values(?,?,?,?,?)'
+                    pool.query(sql3, [sn, password, name, department_sn, post],function(err, result){
+                        if(err) throw err
+                        response.json({code:200, msg:'添加成功'})
+                    })
+                 }else{
+                     response.json({code:502, msg:'部门编号不存在'})
+                 }
+             })
+         }
+     })
+ })
+
+ /**
+  * 2.2删除员工
+  * 删除员工同时删除该员工的报销单
+  * 根据外键约束检查机制删除顺序如下
+  * 报销单明细与报销单处理记录->报销单->员工
+  */
+ server.post('/employee/delete', function(request, response){
+     let sn = request.body.sn
+     let sql = 'delete from employee where sn=?'
+     pool.query(sql, [sn], function(err, result){
+         if(err) throw err
+         response.json({code:200, msg:'删除成功'})
+     })
+ })
+
+ /**
+  * 2.3按部门查找员工
+  */
+ server.post('/employee/departlist',function(request, response){
+     let department_sn = request.body.department_sn
+     let sql = 'select employee.sn, employee.name, employee.department_sn, employee.post, department.name as departmentName from employee,department where employee.department_sn = department.sn and employee.department_sn=?'
+     pool.query(sql, [department_sn,department_sn],function(err, result){
+         if(err) throw err
+         let resultData = {employees:null}
+         resultData.employees = result
+         response.json(resultData)
+     })
+ })
+
+ /**
+  * 2.4查找所有员工
+  */
+ server.get('/employee/all',function(request, response){
+    let sql = 'select employee.sn, employee.name, employee.department_sn, employee.post, department.name as departmentName from employee,department where employee.department_sn = department.sn'
+    pool.query(sql, function(err, result){
+        if(err) throw err
+        let resultData = {employees:null}
+        resultData.employees = result
+        response.json(resultData)
+    })
+ })
+
+ /**
+  * 2.5按员工编号查找员工信息
+  */
+ server.post('/employee/info',function(request,response){
+     let sn = request.body.sn
+     let sql = 'select employee.sn, employee.name, employee.department_sn, employee.post, department.name as departmentName from employee,department where employee.department_sn = department.sn and employee.sn=?'
+     pool.query(sql, [sn], function(err, result){
+         if(err) throw err
+         response.json({employee:result[0]})
+     })
+ })
+
+ /**
+  * 2.6修改员工信息
+  */
+ server.post('/employee/update',function(request,response){
+     let sn = request.body.sn
+     let department_sn = request.body.department_sn
+     let post = request.body.post
+     if(department_sn == ''){
+        response.json({code:401, msg:'请输入部门编号'})
+        return
+    }
+    if(post == ''){
+       response.json({code:402, msg:'请输入职务'})
+       return
+   }
+     let sql = 'update employee set department_sn=?, post=? where sn=?'
+     pool.query(sql, [department_sn,post,sn], function(err,result){
+         if(err) throw err
+         response.json({code:200, msg:'修改成功'})
+     })
  })
